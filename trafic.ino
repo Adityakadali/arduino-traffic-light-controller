18e10ae010be48a7f528cfd4b7e9a0f7dc839336
Adityakadali <adityakadali00@gmail.com>
2017-03-23 19:42:18 +0530

    Add files via upload

---
+++ trafic.ino
@@ -0,0 +1,26 @@
+#define r 13
+#define y 12
+#define g 11
+void setup() {
+  
+pinMode(r,OUTPUT);
+pinMode(y,OUTPUT);
+pinMode(g,OUTPUT);
+}
+
+void loop() {
+ digitalWrite(g,HIGH);
+ delay(9000);
+ digitalWrite(y,HIGH);
+ delay(1000);
+ digitalWrite(g,LOW);
+ digitalWrite(y,LOW);
+ digitalWrite(r,HIGH);
+ delay(4000);
+ digitalWrite(y,HIGH);
+ delay(1000);
+ digitalWrite(r,LOW);
+ digitalWrite(y,LOW);
+ 
+
+}
